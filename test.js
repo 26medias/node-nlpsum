6b233e6ed2bf9b6d184c1f4a56b85151c8523e69
Julien <julien@twenty-six-medias.com>
2014-02-15 18:40:57 -0200

    Updated to complete fractal implementation

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ var nlpsum		= require('nlpsum').main;
 var sum = new nlpsum();
 
 toolbox.file.read("example.txt", function(content) {
-	var summary = sum.fractal(content, 40)
-	toolbox.file.write("example-summary.txt", summary);
+	var summary = sum.fractal(content, 5);
+	console.log("summary data",JSON.stringify(summary.text, null, 4));
+	toolbox.file.write("example-data.json", JSON.stringify(summary, null, 4));
+	toolbox.file.write("example-summary.txt", summary.text);
 });
